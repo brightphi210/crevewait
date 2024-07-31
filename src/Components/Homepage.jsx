03857774b933bf7fe464900aca1f9331cea1030b
Bright Philip <chibuzorphilip2001@gmail.com>
2024-07-31 17:06:34 +0100

    Latest

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import img from '../Images/ss.png'
 import me from '../Images/c (1).png'
 import me1 from '../Images/c (2).png'
@@ -14,56 +14,90 @@ import { PiHandshakeLight } from 'react-icons/pi'
 import { IoCheckmarkDoneCircleOutline, IoFlashOutline } from 'react-icons/io5'
 
 const Homepage = () => {
+
+    
+    const [isLoading, setIsLoading] = useState(false)
+    const [email, setEmail] = useState('')
+    const [fullname, setFullname] = useState('')
+    
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        setIsLoading(true)
+        try {
+          const response = await fetch('https://creve.store/waitlist/', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              email,
+              fullname,
+            })
+          })
+      
+          if (response.ok || response.status === 201) {
+            const data = await response.json()
+            console.log(data);
+            setIsLoading(false)
+            document.getElementById('my_modal_1').showModal()
+            document.getElementById('my_modal_3').close()
+          } else {
+            setIsLoading(false)
+            const data = await response.json()
+            console.log('There was an error');
+          }
+        } catch (error) {
+          console.log(error);
+          setIsLoading(false)
+        }
+      }
   return (
   <div>
 
-    <div className='lg:w-full flex z-50 fixed w-full border border-slate-200 bg-white left-0 right-0 gap-5 items-center m-auto py-3 lg:px-10 px-3 backdrop-filter backdrop-blur-3xl bg-opacity-80'>
-        <div className='lg:w-10 lg:h-10 w-8 h-8'>
-            <img src={logo} alt="" />
+        <div className='lg:w-full flex z-50 fixed w-full border border-slate-200 bg-white left-0 right-0 gap-5 items-center m-auto py-3 lg:px-10 px-3 backdrop-filter backdrop-blur-3xl bg-opacity-80'>
+            <div className='lg:w-10 lg:h-10 w-8 h-8'>
+                <img src={logo} alt="" />
+            </div>
+            <ul className='flex ml-auto gap-3 items-center'>
+                <a href="#why"><li className='text-sm'>Why Creve</li></a>
+                <a href="#about"><li className='text-sm'>About</li></a>
+                <button className='text-sm bg-white border border-neutral-400 text-black px-5 py-2 rounded-full' onClick={()=>document.getElementById('my_modal_3').showModal()}>Join</button>
+            </ul>
         </div>
-        <ul className='flex ml-auto gap-3 items-center'>
-            <a href="#why"><li className='text-sm'>Why Creve</li></a>
-            <a href="#about"><li className='text-sm'>About</li></a>
-            <button className='text-sm bg-white border border-neutral-400 text-black px-5 py-2 rounded-full' onClick={()=>document.getElementById('my_modal_3').showModal()}>Join</button>
-        </ul>
-    </div>
+
+        <div className='lg:px-[10rem] px-5 bg-neutral-100'>
+        
+        <div className='flex justify-center m-auto items-center lg:pt-[10rem] pt-[6rem]'>
+            <div className='text-center'>
+
+            <div className='flex w-fit items-center m-auto mb-4 justify-center border border-green-800 lg:py-3 py-2 lg:px-5 px-5 rounded-full lg:gap-4 gap-2 '>
+                <button className='bg-accent text-white lg:text-sm text-xs py-2 px-4 rounded-full'>New</button>
+                <p className='lg:text-base text-xs'>Join to get notified when we launch.</p>
+            </div>
 
 
+            <div className='py-10 pb-5'>
 
-    <div className='lg:px-[10rem] px-5 bg-neutral-100'>
-      
-      <div className='flex justify-center m-auto items-center lg:pt-[10rem] pt-[6rem]'>
-        <div className='text-center'>
+                <h2 className='lg:text-7xl text-4xl font-medium'>Find all forms <br className='lg:block hidden'/> of <span className='font-bold text-accent'>talent</span>  near you</h2>
+                <p className='pt-5 lg:text-lg text-sm'>
+                Creve is a revolutionary platform designed for everyone, <br className='lg:block hidden'/>
+                bridging the gap between clients and talents seamlessly.
+                </p>
+            </div>
 
-          <div className='flex w-fit items-center m-auto mb-4 justify-center border border-green-800 lg:py-3 py-2 lg:px-5 px-5 rounded-full lg:gap-4 gap-2 '>
-            <button className='bg-accent text-white lg:text-sm text-xs py-2 px-4 rounded-full'>New</button>
-            <p className='lg:text-base text-xs'>Join to get notified when we launch.</p>
-          </div>
+            <div>
+                <button className='bg-black text-white w-fit py-3 px-10 rounded-full text-sm' onClick={()=>document.getElementById('my_modal_3').showModal()}>Join Waitlist</button>
+            </div>
 
-
-          <div className='py-10 pb-5'>
-
-            <h2 className='lg:text-7xl text-4xl font-medium'>Find all forms <br className='lg:block hidden'/> of <span className='font-bold text-accent'>talent</span>  near you</h2>
-            <p className='pt-5 lg:text-lg text-sm'>
-              Creve is a revolutionary platform designed for everyone, <br className='lg:block hidden'/>
-              bridging the gap between clients and talents seamlessly.
-            </p>
-          </div>
-
-          <div>
-            <button className='bg-black text-white w-fit py-3 px-10 rounded-full text-sm' onClick={()=>document.getElementById('my_modal_3').showModal()}>Join Waitlist</button>
-          </div>
-
-          <div className='w-full rounded-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] lg:my-20 my-5'>
-            <img src={img} alt="" className='w-full'/>
-          </div>
+            <div className='w-full rounded-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] lg:my-20 my-5'>
+                <img src={img} alt="" className='w-full'/>
+            </div>
+            </div>
         </div>
-      </div>
- 
-    </div>
+    
+        </div>
 
-
-    <h2 className='text-center 2xl:text-4xl text-2xl pb-4 pt-10' data-aos="fade-up" data-aos-duration="1000">A whole world of  <br /> <span className='mycolor font-bold'>talent</span> at your fingertips</h2>
+        <h2 className='text-center 2xl:text-4xl text-2xl pb-4 pt-10' data-aos="fade-up" data-aos-duration="1000">A whole world of  <br /> <span className='mycolor font-bold'>talent</span> at your fingertips</h2>
         <div className='flex lg:py-10 py-5 2xl:grid 2xl:grid-cols-3 rounded-2xl xl:grid xl:grid-cols-3 lg:grid lg:grid-cols-2 flex-col lg:px-[10rem] 2xl:px-[15rem] xl:px-[10rem]  justify-between px-5 lg:gap-10 gap-5'>
            
            <div className='bg-neutral-100 p-5 rounded-lg' data-aos="fade-up" data-aos-duration="1000">
@@ -91,8 +125,7 @@ const Homepage = () => {
            </div>
         </div>
 
-
-    <div className='2xl:px-[15rem] xl:px-[10rem] lg:px-[10rem] px-5 lg:pt-20 pt-10' id='about'>
+        <div className='2xl:px-[15rem] xl:px-[10rem] lg:px-[10rem] px-5 lg:pt-20 pt-10' id='about'>
             <p className='border-b border-green-500 pb-2 mb-3 w-fit text-2xl'>About Us</p>
             <h2 className='text-justify 2xl:text-xk xl:text-lg lg:text-lg text-sm'>
                 Creve was born out of a simple idea: to create a 
@@ -226,25 +259,24 @@ const Homepage = () => {
         </div>
 
         <div className='flex lg:flex-row flex-col gap-6  2xl:px-[15rem] xl:px-[10rem] lg:px-[10rem] bg-black text-white p-10'>
-        <div className='lg:w-20 lg:h-20 w-14 h-14'>
-            <img src={logo} alt="" />
-        </div>
+            <div className='lg:w-20 lg:h-20 w-14 h-14'>
+                <img src={logo} alt="" />
+            </div>
 
-        <div className='lg:ml-auto'>
-            <h2 className='text-sm pb-5'>Contact Us</h2>
-            <p className='text-sm pb-2'>For any questions or inquiries, contact:</p>
-            <a href='mailto:africacreve@gmail.com' className='text-blue-300 underline'><p>africacreve@gmail.com</p></a>
+            <div className='lg:ml-auto'>
+                <h2 className='text-sm pb-5'>Contact Us</h2>
+                <p className='text-sm pb-2'>For any questions or inquiries, contact:</p>
+                <a href='mailto:africacreve@gmail.com' className='text-blue-300 underline'><p>africacreve@gmail.com</p></a>
+            </div>
         </div>
-        </div>
-
 
       <dialog id="my_modal_3" className="modal">
             <div className="modal-box p-10 pt-5 rounded-lg bg-white lg:max-w-[35rem]">
-                <form method="dialog">
+                {/* <form method="dialog"> */}
                     
                     <div className='flex items-center mb-5'>
                         <img src={logo} alt='' className='w-10 h-10'/>
-                        <button className="btn btn-sm btn-circle btn-ghost right-2 top-2 ml-auto text-lg">✕</button>
+                        <button className="btn btn-sm btn-circle btn-ghost right-2 top-2 ml-auto text-lg" onClick={()=>{document.getElementById('my_modal_3').close()}}>✕</button>
                     </div>
 
 
@@ -257,24 +289,26 @@ const Homepage = () => {
                     </div>
 
 
-                    <div className='pt-10'>
-                        <p className='lg:text-sm text-xs pb-1'>Full Name: </p>
-                        <label className="input input-bordered flex items-center gap-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
-                            <input type="text" className="grow"  placeholder="Enter fullname" />
-                        </label>
-                    </div>
-
-                    <div className='pt-5'>
-                        <p className='lg:text-sm text-xs pb-1'>Email: </p>
-                        <label className="input input-bordered flex items-center gap-2 mb-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
-                            <input type="text" className="grow" placeholder="Email" />
-                        </label>
-                    </div>
-
-                    <p onClick={() => { document.getElementById('my_modal_1').showModal() }} className='bg-green-950 cursor-pointer flex m-auto justify-center  text-white mt-5 lg:p-3 lg:px-10 lg:w-full w-full lg:text-lg p-3 px-16 text-sm rounded-full'>Join the waitlist</p>
                     
+                    <form action="" onSubmit={handleRegister}>
+                        <div className='pt-10'>
+                            <p className='lg:text-sm text-xs pb-1'>Full Name: </p>
+                            <label className="input input-bordered flex items-center gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
+                                <input value={fullname} type="text" onChange={(e)=>setFullname(e.target.value)} required className="grow"  placeholder="Enter fullname" />
+                            </label>
+                        </div>
+
+                        <div className='pt-5'>
+                            <p className='lg:text-sm text-xs pb-1'>Email: </p>
+                            <label className="input input-bordered flex items-center gap-2 mb-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+                                <input value={email} type="text" onChange={(e)=>setEmail(e.target.value)} required className="grow" placeholder="Enter Email" />
+                            </label>
+                        </div>
+
+                        <button type='submit' className='bg-green-950 cursor-pointer flex m-auto justify-center  text-white mt-5 lg:p-3 lg:px-10 lg:w-full w-full lg:text-lg p-3 px-16 text-sm rounded-full'>{isLoading === true ? <span class="loader"></span> : 'Join waitlist'}</button>
+                    </form>
 
                     <div>
                         <p className='text-center text-xs pt-5'>
@@ -282,7 +316,7 @@ const Homepage = () => {
                             We’ll only send you product updates that you can unsubscribe from anytime.
                         </p>
                     </div>
-                </form>
+                {/* </form> */}
             </div>
       </dialog>
 
@@ -297,10 +331,10 @@ const Homepage = () => {
                         <div className='flex m-auto justify-center'>
                             <img src={successImg} alt="" />
                         </div>
-                        <h2 className='text-center'>Request Sent</h2>
-                        <p className='text-center text-xs'>Your details has been recieved join our community</p>
+                        <h2 className='text-center text-2xl font-bold'>Request Successfull</h2>
+                        <p className='text-center lg:text-sm text-xs pt-3 '>Your details has been recieved, <br /> join our community to get latest update from us</p>
 
-                        <Link to={'https://t.me/+nflgbLBXe5xlMjI8'}>
+                        <Link to={'https://chat.whatsapp.com/KuOjvDVM1bfJLXBZYftDpK'}>
                             <button className="btn btn-active text-sm btn-neutral w-9/12 justify-center m-auto mt-8 flex items-center gap-4">Join Community <FaArrowRight /></button>
                         </Link>
                     </div>
